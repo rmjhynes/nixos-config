32c5ac5892afc2267d3bb415349c3af4c373981b
Ryan Hynes <131348960+rmjhynes@users.noreply.github.com>
2024-12-17 16:55:54 +0000

    Imported dotfiles for user config.

--- home.nix
+++ home.nix
@@ -7,12 +7,20 @@
 
   programs.home-manager.enable = true;
 
-  home.file."test.txt".text = "This is a test...";
-
+  # Importing dotfiles from dotfiles github repo.
   home.file = {
-    ".aliases" = {
-      source = dotfiles + "/.aliases";
+
+    "/.config/nvim" = {
+      source = dotfiles + "/nvim";
+      recursive = true;
     };
+
+    ".zshrc".source = dotfiles + "/.zshrc";
+    ".p10k.zsh".source = dotfiles + "/.p10k.zsh";
+    ".aliases".source = dotfiles + "/.aliases";
+    ".vimrc".source = dotfiles + "/.vimrc";
+    ".tmux.conf".source = dotfiles + "/.tmux.conf";
+
   };
 
   home.packages = with pkgs; [
