a16f24d2858a317fee4889819f3cf4768b7d6e7f
Ryan Hynes <131348960+rmjhynes@users.noreply.github.com>
2024-12-19 21:13:43 +0000

    Enabled GNOME instead of KDE.

--- configuration.nix
+++ configuration.nix
@@ -55,11 +55,18 @@
 
   # Enable the X11 windowing system.
   # You can disable this if you're only using the Wayland session.
-  services.xserver.enable = true;
+  #services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
+  #services.displayManager.sddm.enable = true;
+  #services.desktopManager.plasma6.enable = true;
+
+  # Enable GNOME Desktop Environment
+  services.xserver = {
+    enable = true;
+    displayManager.gdm.enable = true;
+    desktopManager.gnome.enable = true;
+  };
 
   # Configure keymap in X11
   services.xserver.xkb = {
