d51e356d27bd32c6827183082de1118459006309
Ryan Hynes <131348960+rmjhynes@users.noreply.github.com>
2025-03-16 14:16:11 +0000

    Added hyprland workspaces and associated keybinds.

--- users/rmjhynes/config/hyprland.nix
+++ users/rmjhynes/config/hyprland.nix
@@ -34,7 +34,7 @@
          pseudotile = true;
          preserve_split = true;
        };
-      
+
       decoration  = {
         rounding = 10;
 
@@ -58,6 +58,20 @@
         };
       };
 
+      # Setup 10 different workspaces
+      workspace = [
+        "1"
+        "2"
+        "3"
+        "4"
+        "5"
+        "6"
+        "7"
+        "8"
+        "9"
+        "10"
+      ];
+
       # Keybinds
       bind = [
         "$mod, RETURN, exec, ghostty"
@@ -75,6 +89,18 @@
         "$mod, right, movefocus, r"
         "$mod, up, movefocus, u"
         "$mod, down, movefocus, d"
+
+        # Switch workspaces
+        "$mod, 1, workspace, 1"
+        "$mod, 2, workspace, 2"
+        "$mod, 3, workspace, 3"
+        "$mod, 4, workspace, 4"
+        "$mod, 5, workspace, 5"
+        "$mod, 6, workspace, 6"
+        "$mod, 7, workspace, 7"
+        "$mod, 8, workspace, 8"
+        "$mod, 9, workspace, 9"
+        "$mod, 0, workspace, 10"
       ];
     };
   };
