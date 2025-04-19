95bc33b6f4bde6e27e52476a7f131912cac50c60
Ryan Hynes <131348960+rmjhynes@users.noreply.github.com>
2025-04-19 21:17:30 +0100

    feat: configure nix formatting with git-hooks.nix

--- flake.nix
+++ flake.nix
@@ -25,6 +25,10 @@
       url = "github:0xc000022070/zen-browser-flake";
     };
 
+    pre-commit-hooks = {
+      url = "github:cachix/git-hooks.nix";
+    };
+
   };
 
   outputs =
@@ -35,11 +39,29 @@
       dotfiles,
       ghostty,
       zen-browser,
+      pre-commit-hooks,
       ...
     }:
     let
       systemIntel = "x86_64-linux";
       systemArm = "aarch64-linux";
+
+      # Setup helper function to install and run pre-commit hooks
+      # for all system architectures
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      forAllSystems =
+        f:
+        nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            inherit system;
+            pkgs = nixpkgs.legacyPackages.${system};
+          }
+        );
+
       pkgs = import nixpkgs {
         inherit systemIntel;
         inherit systemArm;
@@ -67,6 +89,33 @@
 
     in
     {
+      # Pre-commit hooks config using git-hooks.nix
+      checks = forAllSystems (
+        { system, pkgs }:
+        {
+          pre-commit-check = pre-commit-hooks.lib.${system}.run {
+            # Use the current directory as source for checking files
+            src = ./.;
+            # Define which hooks to enable
+            hooks = {
+              # Only using nix formatting
+              nixfmt-rfc-style.enable = true;
+            };
+          };
+        }
+      );
+
+      # devshell to install and run pre-commit hooks
+      devShells = forAllSystems (
+        { system, pkgs }:
+        {
+          default = pkgs.mkShell {
+            inherit (self.checks.${system}.pre-commit-check) shellHook;
+            buildInputs = self.checks.${system}.pre-commit-check.enabledPackages;
+          };
+        }
+      );
+
       # Host machine configurations
       nixosConfigurations = {
 
